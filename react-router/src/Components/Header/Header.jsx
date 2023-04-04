c283ecb4404197d50c16d82872ee51110e662f01
sajim-khan <shossain1427@gmail.com>
2023-04-04 16:23:19 +0600

    added Active route, Loading Spinner and Not Found

--- react-router/src/Components/Header/Header.jsx
+++ react-router/src/Components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './Header.css'
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/friends">Friends</Link>
-        <Link to="/posts">Posts</Link>
+        <ActiveLink to="/">Home</ActiveLink>
+        <ActiveLink to="/about">About</ActiveLink>
+        <ActiveLink to="/contact">Contact</ActiveLink>
+        <ActiveLink to="/friends">Friends</ActiveLink>
+        <ActiveLink to="/posts">Posts</ActiveLink>
       </nav>
     );
 };
 
 export default Header;
+
+
